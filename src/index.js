49e1e4c93a915befa8502b2159ae6b33a7bf8b06
Shrey Gupta <connectwithshrey@gmail.com>
2019-11-08 21:26:36 +0530

    INITIAL

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'index.css';
-import App from 'App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
 import configureStore, {history} from "root/redux/store";
